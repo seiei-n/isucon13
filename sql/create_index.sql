0c29446b79ccf1f66e1cdd15652feb24bfbf6528
Daaaai0809 <dai.tsuruga@80and.co>
2023-11-25 11:45:35 +0900

    unchi

--- sql/create_index.sql
+++ sql/create_index.sql
@@ -1,5 +1,6 @@
 CREATE INDEX `idx_livestream_id_tags` ON `livestream_tags` (`livestream_id`);
-CREATE INDEX `idx_reservation_slot_timestamp` ON `reservation_slots` (`start_at`, `end_at`);
+CREATE INDEX `idx_reservation_slot_start_end` ON `reservation_slots` (`start_at`, `end_at`);
 CREATE INDEX `idx_user_id_livestream_id` ON `ng_words` (`user_id`, `livestream_id`);
 CREATE INDEX `idx_user_id` ON `livestreams` (`user_id`);
 CREATE INDEX `idx_livestream_id_comments` ON `livestream_comments` (`livestream_id`);
+
