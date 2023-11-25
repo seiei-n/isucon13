13e4615771595930f4b63e64202fba5222e4e0ec
Daaaai0809 <dai.tsuruga@80and.co>
2023-11-25 15:29:17 +0900

    コメントアウト削除

--- go/livestream_handler.go
+++ go/livestream_handler.go
@@ -511,24 +511,6 @@ func fillLivestreamResponse(ctx context.Context, tx *sqlx.Tx, livestreamModel Li
 		}
 	}
 
-	// var livestreamTagModels []*LivestreamTagModel
-	// if err := tx.SelectContext(ctx, &livestreamTagModels, "SELECT * FROM livestream_tags WHERE livestream_id = ?", livestreamModel.ID); err != nil {
-	// 	return Livestream{}, err
-	// }
-
-	// tags := make([]Tag, len(livestreamTagModels))
-	// for i := range livestreamTagModels {
-	// 	tagModel := TagModel{}
-	// 	if err := tx.GetContext(ctx, &tagModel, "SELECT * FROM tags WHERE id = ?", livestreamTagModels[i].TagID); err != nil {
-	// 		return Livestream{}, err
-	// 	}
-
-	// 	tags[i] = Tag{
-	// 		ID:   tagModel.ID,
-	// 		Name: tagModel.Name,
-	// 	}
-	// }
-
 	livestream := Livestream{
 		ID:           livestreamModel.ID,
 		Owner:        owner,
