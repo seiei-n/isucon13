3c0a86c900f68063eff4d22829ee3cd9be54b1f4
seiei-n <solehamugoiyo@gmail.com>
2023-11-25 10:31:37 +0900

    pprof 追加

--- go/main.go
+++ go/main.go
@@ -11,6 +11,8 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
+	_ "net/http/pprof"
+	"runtime"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -119,6 +121,12 @@ func initializeHandler(c echo.Context) error {
 }
 
 func main() {
+	runtime.SetBlockProfileRate(1)
+	runtime.SetMutexProfileFraction(1)
+	go func() {
+        log.Println(http.ListenAndServe("0.0.0.0:6060", nil))
+    }()
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 	e := echo.New()
 	e.Debug = true
 	e.Logger.SetLevel(echolog.DEBUG)
